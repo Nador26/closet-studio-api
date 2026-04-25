a83202c4df85e498f296c51187fdb0895badb284
Nador26 <nada.atia26@gmail.com>
2026-04-25 23:14:36 +0200

    increase body limit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "50mb" }));
 
 const anthropic = new Anthropic({
   apiKey: process.env.ANTHROPIC_API_KEY,
